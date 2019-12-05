0b3a6d3332781555df2c46785a1d21892618fa7c
juliankominiak-ps <56292505+juliankominiak-ps@users.noreply.github.com>
2019-12-05 23:25:24 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(multiply(2,3));
-    }
-    public static int multiply(int x, int y) {
-        return x*y;
     }
 }
